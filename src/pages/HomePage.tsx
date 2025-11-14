083d799a1fc510d0ccd543953e4e174e4443bdca
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 03:17:28 +0100

    Updated HomePage.tsx

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import Hero from '../components/Hero';
+import WhyVisit from '../components/WhyVisit';
+import Experiences from '../components/Experiences';
+import Hotels from '../components/Hotels';
+import Events from '../components/Events';
+import CallToAction from '../components/CallToAction';
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <WhyVisit />
+      <Experiences />
+      <Hotels />
+      <Events />
+      <CallToAction />
+    </>
+  );
+}
