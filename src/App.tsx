fcade845c776a31355e90be47054760cbcbb8e53
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 03:24:54 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import HeritagePage from './pages/HeritagePage';
+import ExperiencesPage from './pages/ExperiencesPage'; // Added
+import HotelsPage from './pages/HotelsPage'; // Added
+import EventsPage from './pages/EventsPage'; // Added
 
 // This component ensures the Header and Footer are on every page
 const AppLayout = () => (
   <div className="min-h-screen">
     <Header />
     <main>
-      <Outlet /> {/* This is where your page content (HomePage, HeritagePage, etc.) will be rendered */}
+      <Outlet /> {/* This is where your page content will be rendered */}
     </main>
     <Footer />
   </div>
@@ -21,12 +24,12 @@ function App() {
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/heritage" element={<HeritagePage />} />
-        {/* You can add more pages here later, like: */}
-        {/* <Route path="/hotels" element={<HotelsPage />} /> */}
-        {/* <Route path="/events" element={<EventsPage />} /> */}
+        <Route path="/experiences" element={<ExperiencesPage />} /> {/* Added */}
+        <Route path="/hotels" element={<HotelsPage />} /> {/* Added */}
+        <Route path="/events" element={<EventsPage />} /> {/* Added */}
       </Route>
     </Routes>
   );
 }
 
-export default App; 
+export default App;
