e8120d82a3d8e87602aab75ece93efb40e2f2fb7
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 14:10:55 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import HomePage from './pages/HomePage.tsx';
-import ExperiencesPage from './pages/ExperiencesPage.tsx';
-import HeritagePage from './pages/HeritagePage.tsx';
-import HotelsPage from './pages/HotelsPage.tsx';
-import EventsPage from './pages/EventsPage.tsx';
+import SeeDoPage from './pages/SeeDoPage.tsx';
+import EatDrinkPage from './pages/EatDrinkPage.tsx';
+import AccommodationPage from './pages/AccommodationPage.tsx';
+import PartnersPage from './pages/PartnersPage.tsx';
 import PlanTripPage from './pages/PlanTripPage.tsx';
 import HotelDetailPage from './pages/HotelDetailPage.tsx';
 
@@ -17,12 +17,12 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
-          <Route path="experiences" element={<ExperiencesPage />} />
-          <Route path="heritage" element={<HeritagePage />} />
-          <Route path="hotels" element={<HotelsPage />} />
-          <Route path="hotels/:id" element={<HotelDetailPage />} />
-          <Route path="events" element={<EventsPage />} />
+          <Route path="see-do" element={<SeeDoPage />} />
+          <Route path="accommodation" element={<AccommodationPage />} />
+          <Route path="accommodation/:id" element={<HotelDetailPage />} />
+          <Route path="eat-drink" element={<EatDrinkPage />} />
           <Route path="plan-trip" element={<PlanTripPage />} />
+          <Route path="partners" element={<PartnersPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
