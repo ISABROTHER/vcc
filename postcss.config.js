2a51a637804367bc3b5dab1f73667b988f677bc7
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 09:24:10 +0100

    Updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}', // I also added your new pages folder here
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'), // This plugin is now added
-  ],
 };
