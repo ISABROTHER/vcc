adee7e0caa692b33e78fbcba03c302e86fbc0531
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 09:30:38 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}', // This includes your pages
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'), // This is required for line-clamp
-  ],
+  plugins: [],
 };
+ 
