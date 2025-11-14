0e9b3fdf24c8a0e6192aff273a9d34e8fc795b32
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 10:42:06 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,26 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Hero from './components/Hero';
+import WhyVisit from './components/WhyVisit';
+import Experiences from './components/Experiences';
+import Heritage from './components/Heritage';
+import Hotels from './components/Hotels';
+import Events from './components/Events';
+import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import HeritagePage from './pages/HeritagePage';
-import ExperiencesPage from './pages/ExperiencesPage'; // Added
-import HotelsPage from './pages/HotelsPage'; // Added
-import EventsPage from './pages/EventsPage'; // Added
-import HotelDetailPage from './pages/HotelDetailPage'; // IMPORT THIS NEW PAGE
-
-// This component ensures the Header and Footer are on every page
-const AppLayout = () => (
-  <div className="min-h-screen">
-    <Header />
-    <main>
-      <Outlet /> {/* This is where your page content will be rendered */}
-    </main>
-    <Footer />
-  </div>
-);
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/heritage" element={<HeritagePage />} />
-        <Route path="/experiences" element={<ExperiencesPage />} />
-        <Route path="/hotels" element={<HotelsPage />} />
-        <Route path="/hotels/:id" element={<HotelDetailPage />} /> {/* ADD THIS NEW ROUTE */}
-        <Route path="/events" element={<EventsPage />} />
-      </Route>
-    </Routes>
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <WhyVisit />
+      <Experiences />
+      <Heritage />
+      <Hotels />
+      <Events />
+      <CallToAction />
+      <Footer />
+    </div>
   );
 }
 
