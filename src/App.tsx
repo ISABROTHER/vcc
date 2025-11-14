3c1267cfe241286599594ea1d1c4d188fdda22f2
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-14 10:04:08 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HeritagePage from './pages/HeritagePage';
 import ExperiencesPage from './pages/ExperiencesPage'; // Added
 import HotelsPage from './pages/HotelsPage'; // Added
 import EventsPage from './pages/EventsPage'; // Added
+import HotelDetailPage from './pages/HotelDetailPage'; // IMPORT THIS NEW PAGE
 
 // This component ensures the Header and Footer are on every page
 const AppLayout = () => (
@@ -24,9 +25,10 @@ function App() {
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/heritage" element={<HeritagePage />} />
-        <Route path="/experiences" element={<ExperiencesPage />} /> {/* Added */}
-        <Route path="/hotels" element={<HotelsPage />} /> {/* Added */}
-        <Route path="/events" element={<EventsPage />} /> {/* Added */}
+        <Route path="/experiences" element={<ExperiencesPage />} />
+        <Route path="/hotels" element={<HotelsPage />} />
+        <Route path="/hotels/:id" element={<HotelDetailPage />} /> {/* ADD THIS NEW ROUTE */}
+        <Route path="/events" element={<EventsPage />} />
       </Route>
     </Routes>
   );
