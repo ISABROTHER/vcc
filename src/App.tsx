bb54c8dc0f5593a0054268dbf3d8fa2fd316c918
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-17 00:26:05 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
-import { Outlet } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AccommodationPage from './pages/AccommodationPage';
+import HotelDetailPage from './pages/HotelDetailPage';
+import SeeDoPage from './pages/SeeDoPage';
+import EatDrinkPage from './pages/EatDrinkPage';
+import TouristInfoPage from './pages/TouristInfoPage';
+import PartnersPage from './pages/PartnersPage';
+import PlanTripPage from './pages/PlanTripPage';
+import { useLayoutEffect } from 'react';
+import BottomNav from './components/BottomNav';
 
-function App() {
+// Wrapper to scroll to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <BrowserRouter>
+      <ScrollToTop />
       <Header />
-      <main className="flex-grow">
-        <Outlet />
+      {/* Add padding-bottom on mobile to avoid overlap with sticky nav */}
+      <main className="pb-16 sm:pb-0">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/accommodation" element={<AccommodationPage />} />
+          <Route path="/accommodation/:id" element={<HotelDetailPage />} />
+          <Route path="/see-do" element={<SeeDoPage />} />
+          <Route path="/eat-drink" element={<EatDrinkPage />} />
+          <Route path="/tourist-info" element={<TouristInfoPage />} />
+          <Route path="/partners" element={<PartnersPage />} />
+          <Route path="/plan-trip" element={<PlanTripPage />} />
+        </Routes>
       </main>
       <Footer />
-    </div>
+      <BottomNav />
+    </BrowserRouter>
   );
 }
-
-export default App; 
