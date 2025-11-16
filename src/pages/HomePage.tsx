5c5686d0a6e535138349d664d9c31b1a023f81b8
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-16 22:53:04 +0100

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ const gridItems = [
     description: 'Hotels, guesthouses and unique local stays.',
     icon: Bed,
     href: '/accommodation',
-    color: 'bg-tea-50',
+    color: 'bg-cream-50',
   },
   {
     title: 'Food & Drinks',
