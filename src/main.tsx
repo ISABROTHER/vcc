65d2ea1b96db9063ab8a02640d6217e91da781b9
ISABROTHER <ishimpatrickjohnston@gmail.com>
2025-11-25 02:16:58 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import PartnersPage from './pages/PartnersPage.tsx';
 import PlanTripPage from './pages/PlanTripPage.tsx';
 import HotelDetailPage from './pages/HotelDetailPage.tsx';
 import TouristInfoPage from './pages/TouristInfoPage.tsx';
+import TransportationPage from './pages/TransportationPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -25,6 +26,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="plan-trip" element={<PlanTripPage />} />
           <Route path="tourist-info" element={<TouristInfoPage />} />
           <Route path="partners" element={<PartnersPage />} />
+          <Route path="transportation" element={<TransportationPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
